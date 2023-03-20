adef63eb74ef319039a593b6b6b1f7cc4c153e6d
saranafisa <sara_nafisa@protonmail.com>
2023-03-20 10:40:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function countDown(number){
+    
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+      }
+}
+
+function writeCards(names){
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
